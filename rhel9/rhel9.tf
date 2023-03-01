96ffa5d12c43656fc17223354b5bad3bc8a75467
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-01 08:01:38 +0000

    Updates

--- rhel9/rhel9.tf
+++ rhel9/rhel9.tf
@@ -28,7 +28,6 @@ resource "aws_instance" "computer" {
 
   provisioner "remote-exec" {
     inline = [
-      "echo \"if [[ -t 0 && $- = *i* ]]; then stty -ixon; fi\" >> /home/${var.username}/.bashrc",
       "sudo sed -i -E \"s/#?AllowTcpForwarding no/AllowTcpForwarding yes/\" /etc/ssh/sshd_config",
       "sudo sed -i -E \"s/#?PasswordAuthentication no/PasswordAuthentication yes/\" /etc/ssh/sshd_config",
       "sudo bash -c 'echo \"%wheel ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers'",
