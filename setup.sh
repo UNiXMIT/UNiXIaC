df4d3c602aea2f3562906aa4fc6b8a7a5657dcee
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-02 17:13:48 +0000

    Update docker script

--- setup.sh
+++ setup.sh
@@ -10,8 +10,23 @@ podman exec -it ROCKY python3 -m pip install install cryptography -U
 podman exec -it ROCKY gh auth login
 podman exec -it ROCKY gh repo clone UNiXMIT/UNiXIaC
 podman exec -it ROCKY bash chmod +x UNiXIaC/*.sh
+if [ ! -f support.pem ]; then
+    echo
+    echo "support.pem and pfsso*.zip must be located in your current directory."
+    echo "Please correct this and re-run the script."
+    echo
+    exit 1
+elif  [ ! -f pfsso*.zip ]; then
+    echo
+    echo "support.pem and pfsso*.zip must be located in your current directory."
+    echo "Please correct this and re-run the script."
+    echo
+    exit 1
+fi
+podman cp support.pem ROCKY:/UNiXIaC
+podman cp pfsso*zip ROCKY:/UNiXIaC
 podman exec -it ROCKY unzip /UNiXIaC/pfsso*.zip -d /UNiXIaC
-podman exec -it ROCKY python3 -m pip install --upgrade /UNiXIaC/pfsso-0.9.4
+podman exec -it ROCKY bash -c "python3 -m pip install --upgrade /UNiXIaC/pfsso*"
 podman exec -it ROCKY bash /UNiXIaC/sso.sh
 podman exec -it ROCKY mkdir /root/.ssh
 podman exec -it ROCKY touch /root/.ssh/known_hosts
