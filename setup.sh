e220fe04949a6706cc6329ed701ee5a44b00f5b6
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-03 13:23:02 +0000

    Updates

--- setup.sh
+++ setup.sh
@@ -3,12 +3,10 @@ podman pull rockylinux:8
 podman run -it -d --name ROCKY rockylinux:8
 podman exec -it ROCKY dnf update -y
 podman exec -it ROCKY dnf install epel-release -y
-podman exec -it ROCKY dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 podman exec -it ROCKY dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-podman exec -it ROCKY dnf install terraform git python39 python3-pip ansible sshpass gh unzip -y
+podman exec -it ROCKY dnf install terraform git python39 python3-pip ansible sshpass unzip -y
 podman exec -it ROCKY python3 -m pip install install cryptography -U
-podman exec -it ROCKY gh auth login
-podman exec -it ROCKY gh repo clone UNiXMIT/UNiXIaC
+podman exec -it ROCKY git clone https://github.com/UNiXMIT/UNiXIaC.git
 podman exec -it ROCKY bash chmod +x UNiXIaC/*.sh
 if [ ! -f support.pem ]; then
     echo
--- terraform.sh
+++ terraform.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export ACTION=$1
 export INSTANCE=$2
+export TFUSR=$3
+export TFPASS=$4
 
+git pull
 if [ -z "$INSTANCE" ] || [ -z "$ACTION" ]; then
     echo "No Action or Instance specified!"
     echo "Usage: terraform.sh create rhel9"
@@ -11,7 +14,7 @@ elif [ $ACTION = "destroy" ]; then
 elif [ $ACTION = "create" ]; then
     terraform -chdir=$2 init
     terraform -chdir=$2 plan
-    terraform -chdir=$2 apply -auto-approve
+    terraform -chdir=$2 apply -auto-approve -var username=$TFUSER -var password=$TFPASS
 else
     echo "Invalid Action specified!"
     echo "Usage: terraform.sh create rhel9"
