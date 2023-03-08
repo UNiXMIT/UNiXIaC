6b91e7fdb1ae7812e209cb30e4553c4d31b75dec
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-08 20:21:17 +0000

    Add aws_profile

--- sso.sh
+++ sso.sh
@@ -3,6 +3,3 @@ export AWSEXP=$(echo $(( ($(date +%s) - $(stat ~/.aws/credentials -c %Y)) / 60 )
 if [ $AWSEXP -ge 60 ]; then
     pfsso -a -s 1 -2 ask
 fi
-cat ~/.aws/credentials | grep aws_access_key_id
-cat ~/.aws/credentials | grep aws_secret_access_key
-cat ~/.aws/credentials | grep aws_session_token
