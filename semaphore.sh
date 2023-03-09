bfa0269f621d793eea504912e5872c0845d42740
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-09 21:09:36 +0000

    Update setup script

--- semaphore.sh
+++ semaphore.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Install Intructions
 # sudo (apt/yum/zypper) install -y curl podman
-# sudo curl -s https://raw.githubusercontent.com/UNiXMIT/UNiXIaC/main/semaphore.sh | bash
+# curl -s https://raw.githubusercontent.com/UNiXMIT/UNiXIaC/main/semaphore.sh | bash
 podman pull rockylinux:9
 podman run -itd --name semaphore -p 8181:3000 rockylinux:9
 podman exec -it semaphore bash -c "dnf epel-release -y"
 podman exec -it semaphore bash -c "dnf install ansible wget git python3 python3-pip tmux unzip -y"
 podman exec -it semaphore bash -c "python3 -m pip install boto3"
 podman exec -it semaphore bash -c "wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm"
-podman exec -it semaphore bash -c "dnf install semaphore_2.8.77_linux_amd64.rpm"
+podman exec -it semaphore bash -c "dnf install semaphore*.rpm"
 podman exec -it semaphore bash -c "cd /root && semaphore setup"
-podman exec -it semaphore bash -c "cd /root && semaphore service --config=./config.json"
-podman exec -it semaphore bash -c "tmux new -d -s semaphore semaphore server --config /root/config.json"
+podman exec -it semaphore bash -c "cd /root && tmux new -d -s semaphore semaphore server --config /root/config.json"
 podman cp pfsso*.zip semaphore:/root
 podman cp support.pem semaphore:/root/.ssh
 podman exec -it semaphore bash -c "chmod 600 /root/.ssh/support.pem"
 podman exec -it semaphore bash -c "cd /root && unzip pfsso*.zip"
-podman exec -it semaphore bash -c "cd /root/pfsso-0.9.4 && python3 -m pip install --upgrade ."
+podman exec -it semaphore bash -c "python3 -m pip install --upgrade /root/pfsso*/"
 podman exec -it semaphore bash -c "wget -P /root https://raw.githubusercontent.com/UNiXMIT/UNiXIaC/main/sso.sh"
 podman exec -it semaphore bash -c "chmod +x /root/sso.sh"
