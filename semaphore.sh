379b246bc7fa6241f2181252a26873e78cf7ca73
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-09 13:09:18 +0000

    Update

--- semaphore.sh
+++ semaphore.sh
@@ -2,7 +2,8 @@
 podman pull rockylinux:9
 podman run -itd --name semaphore -p 8181:3000 rockylinux:9
 podman exec -it semaphore bash -c "mkdir /home/semaphore"
-podman exec -it semaphore bash -c "dnf install ansible-core wget git python3 python3-pip tmux unzip -y"
+podman exec -it semaphore bash -c "dnf epel-releasey"
+podman exec -it semaphore bash -c "dnf install ansible wget git python3 python3-pip tmux unzip -y"
 podman exec -it semaphore bash -c "python3 -m pip install boto3"
 podman exec -it semaphore bash -c "wget -P /home/semaphore/ https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm"
 podman exec -it semaphore bash -c "dnf install semaphore_2.8.77_linux_amd64.rpm"
