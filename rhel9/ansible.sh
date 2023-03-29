5d26a6d77f48bcd4bb0f30f691d14c49bd9e37e3
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-03-29 15:08:39 +0100

    Use ANSIBLE_PERSISTENT_COMMAND_TIMEOUT

--- rhel9/ansible.sh
+++ rhel9/ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
-export ANSIBLE_COMMAND_TIMEOUT=60
+export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=120
 ansible-playbook main.yml -i inventory.ini --tags "default" --extra-vars "@variables.json"
