c81bc03c4ffafc611e37406f38ed33981fe75474
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-04-30 14:14:05 +0100

    psrp

--- semaphore.sh
+++ semaphore.sh
@@ -9,6 +9,8 @@ podman exec -it semaphore bash -c "dnf install ansible wget git python3 python3-
 podman exec -it semaphore bash -c "ansible-galaxy collection install amazon.aws"
 podman exec -it semaphore bash -c "python3 -m pip install boto3"
 podman exec -it semaphore bash -c "python3 -m pip install pexpect"
+podman exec -it semaphore bash -c "python3 -m pip install pypsrp"
+podman exec -it semaphore bash -c "python3 -m pip install pywinrm"
 podman exec -it semaphore bash -c "wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm"
 podman exec -it semaphore bash -c "dnf install semaphore*.rpm"
 podman exec -it semaphore bash -c "cd /root && semaphore setup"
