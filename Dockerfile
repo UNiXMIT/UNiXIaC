9d38bd8fdb47a4131972e66f420cffca9a3ab233
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-06-19 16:14:15 +0100

    Fix typos

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM rockylinux:9
 EXPOSE 3000
 COPY pfsso*.zip /root
 COPY support.pem /root/.ssh
-RUN dnf epel-release -y; \
+RUN dnf install epel-release -y; \
     dnf install ansible wget git python3 python3-pip tmux unzip python-winrm -y; \
     ansible-galaxy collection install amazon.aws; \
     python3 -m pip install boto3 pexpect pypsrp pywinrm; \
-    wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm; \
+    wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.90/semaphore_2.8.90_linux_amd64.rpm; \
     dnf install /root/semaphore*.rpm; \
     cd /root && semaphore setup; \
     chmod 600 /root/.ssh/support.pem; \
--- semaphore.sh
+++ semaphore.sh
@@ -6,12 +6,12 @@ if [ $1="podman" ]
 then
     podman pull rockylinux:9
     podman run -itd --name semaphore -p 8181:3000 rockylinux:9
-    podman exec -it semaphore bash -c "dnf epel-release -y"
+    podman exec -it semaphore bash -c "dnf install epel-release -y"
     podman exec -it semaphore bash -c "dnf install ansible wget git python3 python3-pip tmux unzip python-winrm -y"
     podman exec -it semaphore bash -c "ansible-galaxy collection install amazon.aws"
     podman exec -it semaphore bash -c "python3 -m pip install boto3 pexpect pypsrp pywinrm"
-    podman exec -it semaphore bash -c "wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm"
-    podman exec -it semaphore bash -c "dnf install semaphore*.rpm"
+    podman exec -it semaphore bash -c "wget -P /root https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.90/semaphore_2.8.90_linux_amd64.rpm"
+    podman exec -it semaphore bash -c "dnf install /root/semaphore*.rpm -y"
     podman exec -it semaphore bash -c "cd /root && semaphore setup"
     podman exec -it semaphore bash -c "cd /root && tmux new -d -s semaphore semaphore server --config /root/config.json"
     podman cp pfsso*.zip semaphore:/root
@@ -20,12 +20,12 @@ then
     podman exec -it semaphore bash -c "cd /root && unzip pfsso*.zip"
     podman exec -it semaphore bash -c "python3 -m pip install --upgrade /root/pfsso*/"
 else
-    dnf epel-release -y
+    dnf install epel-release -y
     dnf install ansible wget git python3 python3-pip tmux unzip python-winrm -y
     ansible-galaxy collection install amazon.aws
     python3 -m pip install boto3 pexpect pypsrp pywinrm
-    wget https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.75/semaphore_2.8.77_linux_amd64.rpm
-    dnf install semaphore*.rpm
+    wget https://github.com/ansible-semaphore/semaphore/releases/download/v2.8.90/semaphore_2.8.90_linux_amd64.rpm
+    dnf install /root/semaphore*.rpm -y
     semaphore setup
     tmux new -d -s semaphore semaphore server --config config.json
     cp support.pem ~/.ssh/support.pem
