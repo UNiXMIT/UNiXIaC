6f89bce638d6cc85dc653fe81c5d122d1252c89b
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-11-16 08:53:55 +0000

    Fix Yaml formatting

--- modifyFieldTypes.js
+++ modifyFieldTypes.js
@@ -10,14 +10,19 @@
 
 let observer = new MutationObserver(mutations => {
 	for (const elem of document.querySelectorAll("label")) {
-	  if (elem.textContent.includes("SSO Email")) {
-	    elem.nextSibling.type = 'email';
+		if (elem.textContent.includes("Message (Optional)")) {
+		  elem.parentElement.parentElement.parentElement.parentElement.remove();
+		}
 	  }
-	}
-	for (const elem of document.querySelectorAll("label")) {
-	  if (elem.textContent.includes("SSO Password")) {
-	    elem.nextSibling.type = 'password';
+	  for (const elem of document.querySelectorAll("label")) {
+		if (elem.textContent.includes("SSO Email")) {
+		  elem.nextSibling.type = 'email';
+		}
 	  }
-	}
-});
-observer.observe(document, {childList: true, subtree: true});
+	  for (const elem of document.querySelectorAll("label")) {
+		if (elem.textContent.includes("SSO Password")) {
+		  elem.nextSibling.type = 'password';
+		}
+	  }
+  });
+  observer.observe(document, {childList: true, subtree: true});
