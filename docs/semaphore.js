155a247badc0d87cab35d4e0d63f5671b476183b
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-03-05 09:52:00 +0000

    Update NGINX js

--- docs/semaphore.js
+++ docs/semaphore.js
@@ -4,5 +4,12 @@ let observer = new MutationObserver(mutations => {
           elem.parentElement.parentElement.parentElement.parentElement.remove();
         }
       }
-  });
+    let forkIcons = document.querySelectorAll('.mdi-source-fork');
+    forkIcons.forEach(icon => {
+      const parentSpan = icon.closest('span');
+      if (parentSpan) {
+        parentSpan.style.display = 'hidden';
+      }
+    });
+});
 observer.observe(document, {childList: true, subtree: true});
