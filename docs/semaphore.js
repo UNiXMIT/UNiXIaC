4524377fff9950b27463fbee21687b0bba2bffc6
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-03-05 09:53:12 +0000

    Re-format

--- docs/semaphore.js
+++ docs/semaphore.js
@@ -1,15 +1,15 @@
 let observer = new MutationObserver(mutations => {
     for (const elem of document.querySelectorAll("label")) {
         if (elem.textContent.includes("Message (Optional)")) {
-          elem.parentElement.parentElement.parentElement.parentElement.remove();
+            elem.parentElement.parentElement.parentElement.parentElement.remove();
         }
-      }
+    }
     let forkIcons = document.querySelectorAll('.mdi-source-fork');
     forkIcons.forEach(icon => {
-      const parentSpan = icon.closest('span');
-      if (parentSpan) {
-        parentSpan.style.display = 'hidden';
-      }
+        const parentSpan = icon.closest('span');
+        if (parentSpan) {
+            parentSpan.style.display = 'hidden';
+        }
     });
 });
 observer.observe(document, {childList: true, subtree: true});
