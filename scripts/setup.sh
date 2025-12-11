97100f35207f67804eca87c5ab28ce50a86fde11
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-12-11 11:28:33 +0000

    Fix URLs in scripts and configuration files to remove 'refs/heads' for consistency

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,6 +34,7 @@ sudo timedatectl set-timezone Europe/London
 sudo hostnamectl set-hostname $user
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost ${user}/" /etc/hosts
 echo "if [[ -t 0 && $- = *i* ]]; then stty -ixon; fi" >> /home/$user/.bashrc
+echo 'export PS1="$PS1\[\e]1337;CurrentDir='\''\$(pwd)'\''\a\]"' >> /home/$user/.bash_profile
 if [[ -f /etc/ssh/sshd_config ]]; then
   sudo sed -i -E 's/#?AllowTcpForwarding no/AllowTcpForwarding yes/' /etc/ssh/sshd_config;
   sudo sed -i -E 's/#?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config;
@@ -59,8 +60,6 @@ sudo grep -qxF 'fs.file-max=500000' /etc/sysctl.conf || sudo sh -c 'echo "fs.fil
 # Install Software
 # RHEL
 if [ "$WHICHOS" = "RHEL" ]; then
-  sudo dnf update -y;
-  sudo dnf group install -y "Development Tools";
   . /etc/os-release;
   sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID%.*}.noarch.rpm;
   curl https://packages.microsoft.com/config/rhel/${VERSION_ID%.*}/prod.repo | sudo tee /etc/yum.repos.d/mssql-release.repo
@@ -69,12 +68,15 @@ if [ "$WHICHOS" = "RHEL" ]; then
 export PATH="$PATH:/opt/mssql-tools/bin"
 EOF
   sudo chmod 775 /etc/profile.d/mssql.sh
+  sudo dnf update -y;
+  sudo dnf group install -y "Development Tools";
+  sudo ACCEPT_EULA=Y yum install -y msodbcsql17 mssql-tools
   sudo dnf install -y --skip-broken unixODBC-devel wget curl cronie dos2unix htop libstdc++-devel.i686 libaio-devel glibc-devel glibc-devel.i686 glibc glibc.i686 tcpdump ed tmux openconnect jq python3 python3-pip expect postgresql postgresql-odbc net-tools lsof xterm xauth
   if [[ ${VERSION_ID%.*} -le 8 ]]; then
     sudo dnf install -y --skip-broken spax;
   elif [[ ${VERSION_ID%.*} -ge 8 ]]; then
     sudo dnf remove -y java*
-    sudo dnf install -y --skip-broken java-latest-openjdk libnsl libnsl.i686 libxcrypt libxcrypt.i686 libgcc.i686 ncurses-libs.i686 zlib.i686 webkit2gtk3 PackageKit-gtk3-module systemd-libs systemd-libs.i686 podman buildah
+    sudo dnf install -y --skip-broken java-latest-openjdk libnsl libnsl.i686 libxcrypt libncurses* libxcrypt.i686 libgcc.i686 ncurses-libs.i686 zlib.i686 webkit2gtk3 PackageKit-gtk3-module systemd-libs systemd-libs.i686 podman buildah
   elif [[ ${VERSION_ID%.*} -ge 9 ]]; then
     sudo dnf install -y --skip-broken libxcrypt-compat libxcrypt-compat.i686;
   fi
@@ -88,14 +90,15 @@ elif [ "$WHICHOS" = "UBUNTU" ]; then
   curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
   sudo apt update;
   sudo apt upgrade -y;
+  sudo dpkg --add-architecture amd64;
   sudo dpkg --add-architecture i386;
-  sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools
+  sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18 mssql-tools18
   sudo cat > /etc/profile.d/mssql.sh <<EOF
 #!/bin/bash
-export PATH="$PATH:/opt/mssql-tools/bin"
+export PATH="$PATH:/opt/mssql-tools18/bin"
 EOF
   sudo chmod 775 /etc/profile.d/mssql.sh
-  sudo apt install -y -m build-essential openjdk-21-jdk unixodbc-dev wget curl cron dos2unix htop lib32stdc++6 libstdc++6:i386 libaio-dev libncurses5 apt-file zlib1g:i386 libc6:i386 libgc1 tcpdump ed tmux openconnect jq python3 python3-pip expect postgresql-client odbc-postgresql net-tools lsof pax-utils podman buildah unzip libgtk-3-0;
+  sudo apt install -y -m build-essential openjdk-21-jdk unixodbc-dev wget curl cron dos2unix htop lib32stdc++6 libstdc++6:i386 libaio-dev libncurses* apt-file zlib1g:i386 libc6:i386 libgc1 tcpdump ed tmux openconnect jq python3 python3-pip expect postgresql-client odbc-postgresql net-tools lsof pax-utils podman buildah unzip libgtk-3-0;
   sudo apt-file update;
   export ODBCPATH=/etc
 # SLES
@@ -113,10 +116,14 @@ elif [ "$WHICHOS" = "SLES" ]; then
 export PATH="$PATH:/opt/mssql-tools/bin"
 EOF
   sudo chmod 775 /etc/profile.d/mssql.sh
-  sudo zypper install -y unixODBC-devel wget curl cronie dos2unix htop tcpdump ed tmux jq python3 python3-pip expect postgresql net-tools lsof spax java-17-openjdk libaio-devel libaio-devel-32bit glibc-devel glibc-devel-32bit glibc glibc-32bit libcrypt1-32bit libncurses5-32bit libstdc++6-32bit libgcc_s1-32bit libz1-32bit podman buildah openconnect unzip xauthlibgtk-3-0 gtk3-tools libjasper4 libnotify-tools net-tools-deprecated pam pam-32bit;
+  sudo zypper install -y unixODBC-devel wget curl cronie dos2unix htop tcpdump ed tmux jq python3 python3-pip expect postgresql net-tools lsof spax java-17-openjdk libaio-devel libaio-devel-32bit glibc-devel glibc-devel-32bit glibc glibc-32bit libcrypt1-32bit libncurses5-32bit libstdc++6-32bit libgcc_s1-32bit libz1-32bit podman buildah openconnect unzip xauth libgtk-3-0 gtk3-tools libjasper4 libnotify-tools net-tools-deprecated pam pam-32bit;
   export ODBCPATH=/etc/unixODBC
 fi
 sudo systemctl enable --now podman.socket
+sudo curl -s -o /usr/local/bin/yq_linux_amd64 https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ln -s /usr/lib64/libnsl.so.1 /usr/lib64/libnsl.so 
 
 # Install Oracle Instant Client
 if [ "$WHICHOS" = "RHEL" ]; then
@@ -170,7 +177,7 @@ FILE = /home/$user/sqllib
 COMP = BASE_CLIENT
 INSTALL_TYPE = CUSTOM
 EOF
-/opt/ibm/client/db2setup -r /opt/ibm/db2.linux.rsp
+/opt/ibm/client/db2setup -f sysreq -r /opt/ibm/db2.linux.rsp
 sudo cat > /opt/ibm/db2.sh <<EOF
 #!/bin/bash
 . /home/$user/sqllib/db2profile
@@ -190,7 +197,18 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ibm/mq/lib64
 EOF
 
 # ODBC
+## odbcinst.ini
 sudo cat >> $ODBCPATH/odbcinst.ini <<EOF
+[PostgreSQL ANSI]
+Description=PostgreSQL ODBC driver (ANSI version)
+Driver=psqlodbca.so
+Setup=libodbcpsqlS.so
+
+[PostgreSQL Unicode]
+Description=PostgreSQL ODBC driver (Unicode version)
+Driver=psqlodbcw.so
+Setup=libodbcpsqlS.so
+
 [IBM DB2 ODBC DRIVER]
 Description = DB2 Driver
 Driver = /home/$user/sqllib/lib64/libdb2o.so
@@ -212,6 +230,7 @@ Setup=libodbcpsqlS.so
 EOF
 fi
 
+## odbc.ini
 if [ "$WHICHOS" = "RHEL" ]; then
   sudo cat >> $ODBCPATH/odbc.ini <<EOF
 [oracle]
@@ -299,31 +318,44 @@ cd /home/$user/MFSupport
 [ ! -d "MFSamples" ] && mkdir MFSamples
 [ ! -d "MFInstallers" ] && mkdir MFInstallers
 [ ! -d "MFDataFiles" ] && mkdir MFDataFiles
+[ ! -d "CTF" ] && mkdir CTF
+cd /home/$user/MFSupport/CTF
+[ ! -d "TEXT" ] && mkdir TEXT
+[ ! -d "BIN" ] && mkdir BIN
+
 cd /home/$user/MFSupport/MFScripts
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/setupmf.sh
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/startmf.sh
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/setenvmf.sh
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/mfesdiags.sh
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/formatdumps.sh
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/autopac.sh
-sudo chmod +x setupmf.sh startmf.sh setenvmf.sh formatdumps.sh autopac.sh
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/disableSecurity.sh
+sudo chmod +x setupmf.sh startmf.sh setenvmf.sh formatdumps.sh autopac.sh mfesdiags.sh disableSecurity.sh
+cd /home/$user/MFSupport/CTF
+cur -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/ctf.cfg
 cd /home/$user/MFSuport/MFSamples
 if [ ! -d "JCL" ]; then
   mkdir -m 775 JCL
   mkdir -m 775 JCL/system JCL/catalog JCL/dataset JCL/loadlib
-  curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/JCL.xml
+  curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/JCL.xml
+  curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/docs/es/MFBSI.cfg
+  curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/docs/es/VSE.cfg
 fi
 if [ ! -d "CICS" ]; then
   mkdir -m 775 CICS
   mkdir -m 775 CICS/system CICS/dataset JCL/loadlib
-  curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/CICS.xml
+  curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/CICS.xml
 fi
+mkdir -m 775 /home/products
 touch /home/$user/.Xauthority
 sudo chmod 600 /home/$user/.Xauthority
 sudo chmod -R 775 /home/$user
 cd /home/$user
 
 # CRON Jobs
-(sudo crontab -l ; echo "#0 20 * * * shutdown -h now")| sudo crontab -
+CRONLINE='#0 20 * * * sh -c '\''/sbin/shutdown -h +30 && printf "Shutdown scheduled for $(date -d +30mins)\\nCancel using: sudo shutdown -c" | wall'\'''
+(sudo crontab -l 2>/dev/null; echo "$CRONLINE") | sudo crontab -
 (sudo crontab -l ; echo "@reboot sysctl -p /home/$user/AcuSupport/etc/TCPtuning.conf")| sudo crontab -
 
 # MOTD
