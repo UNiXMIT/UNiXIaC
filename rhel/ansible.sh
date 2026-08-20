975851239ff654ac11a3b44dbb458548d18c9f7e
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2026-08-20 11:14:11 +0100

    Remove obsolete Ansible scripts and variable files for various OS configurations

--- rhel/ansible.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ANSIBLE_HOST_KEY_CHECKING=False
-export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=120
-ansible-playbook main.yml -i inventory.ini --tags "default" --extra-vars "@variables.json"
